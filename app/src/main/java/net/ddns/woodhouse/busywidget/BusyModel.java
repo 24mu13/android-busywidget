8fd05bd444cafb7d6120b766d1dc4238d8c4bb5d
Samuel Algisi <salgisi@agtinternational.com>
2016-09-21 10:32:20 +0200

    reading/writing from db implemented

--- app/src/main/java/net/ddns/woodhouse/busywidget/BusyModel.java
+++ app/src/main/java/net/ddns/woodhouse/busywidget/BusyModel.java
@@ -1,15 +1,35 @@
 package net.ddns.woodhouse.busywidget;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class BusyModel {
 
     private static BusyModel ourInstance = new BusyModel();
+    private static DatabaseReference appRef;
 
     public static BusyModel getInstance() {
         return ourInstance;
     }
 
     private BusyModel() {
-        isBusy = false;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        isBusy = false; // un-initialized
+        appRef = database.getReference("isbusy");
+        appRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                isBusy = dataSnapshot.getValue(Boolean.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // log error to get value
+            }
+        });
     }
 
     public boolean isBusy() {
@@ -17,7 +37,8 @@ public class BusyModel {
     }
 
     public void setBusy(boolean busy) {
-        isBusy = busy;
+//        isBusy = busy;
+        appRef.setValue(busy);
     }
 
     private boolean isBusy;
